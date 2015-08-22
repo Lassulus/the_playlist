c9c103c0a751dcbec723ea19c255acf6764396c9
lassulus <lass@aidsballs.de>
2015-08-22 17:30:04 +0200

    use parallel to download files

--- env.nix
+++ env.nix
@@ -2,9 +2,10 @@ with import <nixpkgs> {}; {
   env = stdenv.mkDerivation {
     name = "playlist-tools";
     buildInputs = [
-      sox
-      pypyPackages.youtube-dl-light
       ffmpeg
+      parallel
+      pypyPackages.youtube-dl-light
+      sox
       vorbisgain
     ];
     SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt";
